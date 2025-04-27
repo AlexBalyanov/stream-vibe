70f6f590db7e0f26c33f1315050f6c2b4959ff80
Alex Balyanov <alex44298@yandex.ru>
2025-04-27 19:54:52 +0500

    сверстал футер, добавил соответствующие jsx компоненты

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -13,6 +13,7 @@ const Button = (props) => {
     isLabelHidden = false,
     iconName,
     iconPosition = "before",
+    hasFillIcon,
   } = props
 
   const isLink = href !== undefined
@@ -25,6 +26,7 @@ const Button = (props) => {
     <Icon
       className="button__icon"
       name={iconName}
+      hasFill={hasFillIcon}
     />
   )
 
